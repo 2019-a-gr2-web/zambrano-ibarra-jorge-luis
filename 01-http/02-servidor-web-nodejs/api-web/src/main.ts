7701c85aac779bd2ddff80a47ae37bc289aec50b
jorgez93 <joluzaib@hotmail.com>
2019-05-07 15:58:45 -0500

    HTML, ETIQUETAS, METADATOS

--- 01-http/02-servidor-web-nodejs/api-web/src/main.ts
+++ 01-http/02-servidor-web-nodejs/api-web/src/main.ts
@@ -4,13 +4,18 @@ const cookieParser = require('cookie-parser');
 import requireActual = jest.requireActual;
 import {join} from "path";
 import {NestExpressApplication} from "@nestjs/platform-express";
+import * as express from 'express';
+import * as favicon from 'serve-favicon';
+import * as path from "path";
 //import
 async function bootstrap() {
   const app = await NestFactory.create(AppModule) as NestExpressApplication;
+  app.use(favicon(path.join(__dirname,'..','publico','Imagenes','ironman.ico')));
   app.use(cookieParser('Me gusta el encebollado'));
   // @ts-ignore
   app.setViewEngine('ejs');
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.use(express.static('publico'));
   await app.listen(3001);
 
 }
